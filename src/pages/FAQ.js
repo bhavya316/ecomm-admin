ad1fb921aa34ebbfad6ef3d0a09219ab5a87317b
bhvaya316 <bhavyajain860@gmail.com>
2026-04-10 15:27:44 +0530

    deploy please

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import AdminLayout from '../components/AdminLayout';
 import axios from 'axios';
 import ReactQuill from 'react-quill-new';
